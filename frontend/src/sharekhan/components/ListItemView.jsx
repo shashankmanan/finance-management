34ad3e76c70fd0e74022b812aa20d79989d6a729
shashank manan <shashankmanan@gmail.com>
2025-03-08 01:58:02 +0530

    frontend sharekhan stocks configured

--- frontend/src/sharekhan/components/ListItemView.jsx
+++ frontend/src/sharekhan/components/ListItemView.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
 
-export default function ListItemView() {
+export default function ListItemView({name,buy_price,invested_amt,quantity}) {
   return (
     <div style={{display:'flex', flexDirection:'row' , justifyContent:"space-evenly",alignItems:"center"}}>
             <div>
-                <h3>Aditya Birla Cap</h3>
-                <h4>10</h4>
+                <h3>{name}</h3>
+                <h4>{quantity}</h4>
             </div>
             <div style={{display:'flex',flexDirection:'row',alignItems:'center'}}>
                 <div style={{display:'flex',flexDirection:"column", margin:"10px"}}>
                     <h4>Invested</h4>
-                    <h4>1000</h4>
+                    <h4>{invested_amt}</h4>
                 </div>
-                <div style={{display:'flex',flexDirection:"column"}}>
+                <div style={{display:'flex',flexDirection:"column",margin:"5px"}}>
                 <h4>Current</h4>
                 <h4>1000</h4>
+                </div>
+                <div style={{display:'flex',flexDirection:"column"}}>
+                <h4>Buy_Price</h4>
+                <h4>{buy_price}</h4>
                 </div>
                 
             </div>
